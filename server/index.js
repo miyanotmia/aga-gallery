77ed377e8a84f125867651258dba5b20db0a7204
Simms <miysimms@amazon.com>
2026-02-16 17:09:05 -0500

    fixed conflicts

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ import productRoutes from "./routes/products.js";
 app.use("/api/products", productRoutes);
 
 //server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 35000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
